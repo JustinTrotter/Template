b94f4ad84d352105e5434c112088175e41259e08
Justin Trotter <trotter.justin@gmail.com>
2016-01-20 23:59:58 -0600

    AutoType and Voice Text

---
+++ Scripts/textAutoType.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class textAutoType : MonoBehaviour {
+
+	public float letterPause = 0.2f;
+	public float sentancePause = 10f;
+
+	private GUIText text;
+	private string message;
+
+	// Use this for initialization
+	void Start () {
+		message = GetComponent<Text>().text;
+		GetComponent<Text>().text = "";
+		StartCoroutine (TypeText ());		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator TypeText(){
+		foreach (char letter in message.ToCharArray()) {
+			GetComponent<Text> ().text += letter;
+			if (GetComponent<AudioSource>() && letter != ' ') {
+				GetComponent<AudioSource>().PlayOneShot (GetComponent<AudioSource>().clip);
+				if (letter == '.')
+					yield return new WaitForSeconds (letterPause * sentancePause);
+				else
+					yield return 0;
+			}
+			yield return new WaitForSeconds (letterPause);
+		}
+	}
+}
